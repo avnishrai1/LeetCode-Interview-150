2903c0dfe4f992d34dc3b360405dadefb76390cd
Avnish Rai <125042182+avnishrai1@users.noreply.github.com>
2023-09-16 18:06:43 +0530

    Time: 45 ms (83.97%), Space: 17.1 MB (85.6%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,21 +1,25 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        #firstly convert all uppercase into lowercase
         isLower = s.lower()
 
+        #secondly filter the alphanumeric values
         alphaNum = ""
+
         for ch in isLower :
             if ch.isalnum() :
                 alphaNum += ch
 
-        startPTR = 0 
+        #finally check the string forwardly and backwardly
+        startPTR = 0
         endPTR = len(alphaNum) - 1
 
         while startPTR < endPTR :
-            if alphaNum[startPTR] != alphaNum[endPTR] :
+            if alphaNum[startPTR] !=  alphaNum[endPTR] :
                 return False
-
+            
             startPTR += 1
             endPTR -= 1
 
-        
         return True
+

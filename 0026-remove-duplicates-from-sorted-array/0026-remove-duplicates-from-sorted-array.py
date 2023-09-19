4d392c10f6afc2d14be0ad48d6811b3249b2651f
Avnish Rai <125042182+avnishrai1@users.noreply.github.com>
2023-09-19 22:12:23 +0530

    Time: 78 ms (90.27%), Space: 18 MB (49.3%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,12 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        #firstly initilaise two pointer l and m 
-        l = 1
-        
-        #now iterate  the pointer through the loop 
-        for m in range(1, len(nums)) :
-            if nums[m] != nums[m - 1] :
-                nums[l] = nums[m]
-                l = l + 1
+        #initialise two pointer, one for  hold and another one for comparsion
+        p = 1
 
-        return l
+        for q in range(1, len(nums)) :
+            if nums[q] != nums[q - 1] :
+                nums[p] = nums[q]
+                p = p + 1
+
+        return p
